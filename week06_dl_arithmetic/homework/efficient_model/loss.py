036613f2719e7cb69073f14ee60f5ab04dc37560
Albert Akopyan <asakopyan@avito.ru>
2026-03-20 15:32:56 +0300

    add fused linear ce loss

--- week06_dl_arithmetic/homework/efficient_model/loss.py
+++ week06_dl_arithmetic/homework/efficient_model/loss.py
@@ -5,6 +5,8 @@ Cross Entropy Loss for Causal LM
 import torch
 import torch.nn as nn
 
+from liger_kernel.transformers import LigerFusedLinearCrossEntropyLoss
+
 
 class CrossEntropyLoss(nn.Module):
     """Fused Linear Cross Entropy for causal LM."""
@@ -14,7 +16,15 @@ class CrossEntropyLoss(nn.Module):
     def __init__(self, ignore_index: int = -100):
         super().__init__()
         self.ignore_index = ignore_index
+        self.fused_liger_ce = LigerFusedLinearCrossEntropyLoss(ignore_index=ignore_index)
 
-    def forward(self,) -> torch.Tensor:
+    def forward(self, hidden_states, lm_head_weight, labels) -> torch.Tensor:
         # TODO: Implement forward pass
-        raise NotImplementedError("TODO: Implement forward pass")
+        shift_hidden = hidden_states[:, :-1, :].contiguous()
+        shift_labels = labels[:, 1:].contiguous()
+
+        shift_hidden = shift_hidden.view(-1, shift_hidden.size(-1))
+        shift_labels = shift_labels.view(-1)
+
+        return self.fused_liger_ce(lm_head_weight, shift_hidden, shift_labels)
+
