1da58954a0f32e495fa86fd59039395dc46a6f04
Albert Akopyan <asakopyan@avito.ru>
2026-03-21 17:33:18 +0300

    memory reduction 17,5 mb

--- week06_dl_arithmetic/homework/efficient_model/swiglu.py
+++ week06_dl_arithmetic/homework/efficient_model/swiglu.py
@@ -14,13 +14,13 @@ from liger_kernel.ops.utils import calculate_settings, ensure_contiguous
 
 
 @triton.jit
-def silu(x):
+def silu(x, alpha):
     # TODO: Replace with x * sigmoid(x * alpha)
-    return x * tl.sigmoid(x)
+    return x * tl.sigmoid(x * alpha)
 
 
 @triton.jit
-def _swiglu_forward_kernel(a_ptr, b_ptr, c_ptr, stride, n_cols: tl.constexpr, BLOCK_SIZE: tl.constexpr):
+def _swiglu_forward_kernel(a_ptr, b_ptr, c_ptr, stride, n_cols: tl.constexpr, BLOCK_SIZE: tl.constexpr, alpha: tl.constexpr, limit: tl.constexpr):
     # a = gate, b = up
     program_id = tl.program_id(0).to(tl.int64)
 
@@ -37,12 +37,13 @@ def _swiglu_forward_kernel(a_ptr, b_ptr, c_ptr, stride, n_cols: tl.constexpr, BL
     b_row = tl.load(b_ptr + col_offsets, mask=mask, other=0)
     # TODO: Add clamping
     # TODO: Replace silu(a_row) * b_row with glu * (b_row + 1)
-    c_row = silu(a_row).cast(b_row.dtype) * b_row
+    c_row = silu(a_row, alpha).cast(b_row.dtype) * (b_row + 1)
+    c_row = tl.maximum(tl.minimum(c_row, limit), -limit)
     tl.store(c_ptr + col_offsets, c_row, mask=mask)
 
 
 @triton.jit
-def _swiglu_backward_kernel(dc_ptr, a_ptr, b_ptr, stride, n_cols: tl.constexpr, BLOCK_SIZE: tl.constexpr):
+def _swiglu_backward_kernel(dc_ptr, a_ptr, b_ptr, stride, n_cols: tl.constexpr, BLOCK_SIZE: tl.constexpr, alpha: tl.constexpr, limit: tl.constexpr):
     program_id = tl.program_id(0).to(tl.int64)
 
     # locate start index
@@ -60,18 +61,24 @@ def _swiglu_backward_kernel(dc_ptr, a_ptr, b_ptr, stride, n_cols: tl.constexpr, 
 
     # recomputation to save memory
     # TODO: Update backward pass for gpt-oss style implementation (formula will be different!)
-    sig_a = tl.sigmoid(a_row)
-    silu_a = a_row * sig_a
-    db_row = dc_row * silu_a
-    da_row = dc_row * (silu_a * (1 - sig_a) + sig_a) * b_row
+    sig_a = tl.sigmoid(a_row * alpha)
+    glu_a = a_row * sig_a
+    y = glu_a * (b_row + 1.0)
+
+    clamp_mask = (y >= -limit) & (y <= limit)
+    dc_clamped = tl.where(clamp_mask, dc_row, 0.0)
+
+    db_row = dc_clamped * glu_a
+    da_row = dc_clamped * (glu_a * (1.0 - sig_a) * alpha + sig_a) * (b_row + 1.0)
 
     tl.store(a_ptr + col_offsets, da_row, mask=mask)
     tl.store(b_ptr + col_offsets, db_row, mask=mask)
+    tl.store(dc_ptr + col_offsets, y, mask=mask)
     # HINT: We're already recomputing values here.
     # Could a third store here help avoid saving something else?
 
 
-def swiglu_forward(a, b):
+def swiglu_forward(a, b, alpha, limit):
     ori_shape = a.shape
 
     n_cols = ori_shape[-1]
@@ -90,11 +97,13 @@ def swiglu_forward(a, b):
         n_cols=n_cols,
         BLOCK_SIZE=BLOCK_SIZE,
         num_warps=num_warps,
+        alpha=alpha,
+        limit=limit
     )
     return a, b, c.view(*ori_shape)
 
 
-def swiglu_backward(a, b, dc):
+def swiglu_backward(a, b, dc, alpha, limit):
     ori_shape = dc.shape
     n_cols = ori_shape[-1]
     dc = dc.view(-1, n_cols)
@@ -110,8 +119,10 @@ def swiglu_backward(a, b, dc):
         n_cols=n_cols,
         BLOCK_SIZE=BLOCK_SIZE,
         num_warps=num_warps,
+        alpha=alpha,
+        limit=limit
     )
-    return a.view(*ori_shape), b.view(*ori_shape)
+    return a.view(*ori_shape), b.view(*ori_shape), dc.view(*ori_shape)
 
 
 class MemoryEfficientSwiGLUMLP(torch.autograd.Function):
@@ -125,12 +136,12 @@ class MemoryEfficientSwiGLUMLP(torch.autograd.Function):
         up = x @ w_up.T
 
         # TODO: Replace with fused swiglu_forward kernel
-        activation_out = swiglu_forward(gate, up, ...)
+        gate, up, activation_out = swiglu_forward(gate, up, alpha, limit)
 
         out = activation_out @ w_down.T
 
         # TODO: Save tensors for backward
-        ctx.save_for_backward()  # TODO: fill this
+        ctx.save_for_backward(x, gate, up, w_gate, w_up, w_down)  # TODO: fill this
         ctx.alpha = alpha
         ctx.limit = limit
 
@@ -140,13 +151,39 @@ class MemoryEfficientSwiGLUMLP(torch.autograd.Function):
     def backward(ctx, grad_output):
         # TODO: Implement backward pass
         # ... unpack, recompute whats needed, d_activation ...
+        x, gate, up, w_gate, w_up, w_down = ctx.saved_tensors
+        alpha = ctx.alpha
+        limit = ctx.limit
+
+        orig_shape = grad_output.shape
+        grad_output = grad_output.reshape(-1, orig_shape[-1])
+        x  = x.reshape(-1, x.shape[-1])
+        # swiglu_activation_out = swiglu_activation_out.reshape(-1, swiglu_activation_out.shape[-1])
 
         # Gradient through activation
         # ... = swiglu_backward(...)
+        grad_activation_down = grad_output @ w_down
 
+        grad_activation_gate_out, grad_activation_up_out, swiglu_activation_out = swiglu_backward(gate, up, grad_activation_down, alpha, limit)
+        swiglu_activation_out = swiglu_activation_out.reshape(-1, swiglu_activation_out.shape[-1])
+
+        grad_w_down = grad_output.T @ swiglu_activation_out
+        
+        
+        grad_w_gate = grad_activation_gate_out.T @ x
+        grad_activation_gate_in = grad_activation_gate_out @ w_gate
+
+        grad_w_up = grad_activation_up_out.T @ x
+        grad_activation_up_in = grad_activation_up_out @ w_up
+        
+        grad_x = grad_activation_gate_in + grad_activation_up_in 
+
+        grad_alpha = None
+        grad_limit = None
+
+        grad_x = grad_x.reshape(orig_shape)
         # Gradients for w_down, w_gate, w_up, dx
-
-        raise NotImplementedError()
+        return grad_x, grad_w_gate, grad_w_up, grad_w_down, grad_alpha, grad_limit
 
 
 class SwiGLUFeedForward(nn.Module):
